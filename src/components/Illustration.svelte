398ca6c2cd440fa0e342bce88784fe65111aed58
samsour <sam.sauer@hotmail.de>
2020-06-12 18:24:03 +0200

    Add illustration component

---
+++ src/components/Illustration.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let name;
+    export let background = false;
+    export let left = false;
+    export let right = false;
+</script>
+
+<template>
+<figure class:background class:left class:right>
+    <img alt="{name}" src="./svg/illustrations/{name}.svg">
+</figure>
+</template>
+
+<style lang="scss">
+figure {
+
+    &.background {
+        position: absolute;
+        z-index: -1;
+    }
+
+    &.left {
+        width: 80%;
+        left: 0;
+    }
+    &.right {
+        width: 80%;
+        right: 0;
+    }
+}
+
+img {
+    width: 100%;
+    height: auto;
+}
+</style>
